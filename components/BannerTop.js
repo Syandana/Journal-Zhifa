b36fd90064749a6d30f6ed92af350e9f61cf5277
Syandana <rizqy.syandana.p@gmail.com>
2020-04-08 17:46:22 +0700

    removed date

--- components/BannerTop.js
+++ components/BannerTop.js
@@ -3,13 +3,12 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 const BannerTop = () => (
   <div className="container">
     <div className="row">
-      <div className="col">
-        <p>15 November 2019</p>
+      <div className="col border py-2">
         <h2 className="text-success">Halo, Wendy!</h2>
         <p>Mau ngapain hari ini?</p>
       </div>
-      <div className="col">
-        <p className="text-right"><AccountCircleIcon style={{ fontSize: 100 }}/></p>
+      <div className="col border">
+        <h2 className="text-right"><AccountCircleIcon style={{ fontSize: 70 }}/></h2>
       </div>
     </div>
   </div>
