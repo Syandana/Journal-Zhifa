c8dd976f56499d23c00f1b5a3ad7ac552153aa69
Syandana <rizqy.syandana.p@gmail.com>
2020-04-08 17:52:22 +0700

    finally it centeredddd

--- components/BannerTop.js
+++ components/BannerTop.js
@@ -3,11 +3,11 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 const BannerTop = () => (
   <div className="container">
     <div className="row">
-      <div className="col border py-2">
+      <div className="col py-2">
         <h2 className="text-success">Halo, Wendy!</h2>
         <p>Mau ngapain hari ini?</p>
       </div>
-      <div className="col border">
+      <div className="col py-2">
         <h2 className="text-right"><AccountCircleIcon style={{ fontSize: 70 }}/></h2>
       </div>
     </div>
