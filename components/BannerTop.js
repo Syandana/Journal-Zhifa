529827b4c7dbee1805d7ef1542b76724648205a7
Syandana <rizqy.syandana.p@gmail.com>
2020-04-08 17:55:54 +0700

    funally centered again

--- components/BannerTop.js
+++ components/BannerTop.js
@@ -3,11 +3,11 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 const BannerTop = () => (
   <div className="container">
     <div className="row">
-      <div className="col py-2">
+      <div className="col my-auto">
         <h2 className="text-success">Halo, Wendy!</h2>
         <p>Mau ngapain hari ini?</p>
       </div>
-      <div className="col py-2">
+      <div className="col my-auto">
         <h2 className="text-right"><AccountCircleIcon style={{ fontSize: 70 }}/></h2>
       </div>
     </div>
