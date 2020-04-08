87f1df908668539b4777092471bd43528e07f13b
Syandana <rizqy.syandana.p@gmail.com>
2020-04-08 10:15:58 +0700

    added some components

---
+++ components/Cards.js
@@ -0,0 +1,23 @@
+const Cards = () => (
+  <div className="container overflow-auto">
+    <div className="row">
+      <div className="col-6">
+        <div class="card">
+          <div class="card-body">
+            <h3 className="card-title">Melankolis</h3>
+          </div>
+        </div>
+      </div>
+      <div className="col-6">
+        <div class="card">
+          <div class="card-body">
+            <h3 className="card-title">Melankolis</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+)
+
+export default Cards
---
+++ components/NavigationBottom.js
@@ -0,0 +1,35 @@
+import BottomNavigation from '@material-ui/core/BottomNavigation'
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import { makeStyles } from '@material-ui/core/styles';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import MenuBookRoundedIcon from '@material-ui/icons/MenuBookRounded';
+import DescriptionRoundedIcon from '@material-ui/icons/DescriptionRounded';
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
+
+const useStyles = makeStyles({
+  root: {
+    width: 500,
+  },
+});
+
+function NavigationBottom() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState('recents');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="fixed-bottom">
+      <BottomNavigation value={value} onChange={(event, newValue) => { setValue(newValue); }} showLabels className={classes.root}>
+        <BottomNavigationAction label="Beranda" icon={<HomeRoundedIcon />} />
+        <BottomNavigationAction label="Jurnal saya" icon={<MenuBookRoundedIcon />} />
+        <BottomNavigationAction label="Artikel" icon={<DescriptionRoundedIcon />} />
+        <BottomNavigationAction label="Akun" icon={<AccountCircleRoundedIcon />} />
+      </BottomNavigation>
+    </div>
+  )
+};
+
+export default NavigationBottom
