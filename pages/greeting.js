0eae7c751cf7a904c12d0b9cbba4a183bc10911f
Syandana <rizqy.syandana.p@gmail.com>
2020-04-09 12:37:32 +0700

    added initial greeting page

--- pages/greeting.js
+++ pages/greeting.js
@@ -16,28 +16,17 @@ function Index() {
   return (
     <Layout>
       <div className="container">
-        <div className="row">
-          <div className="col">
-            <h2 className="text-center"><AccountCircleIcon style={{ fontSize: 70 }} /></h2>
-          </div>
-        </div>
         <div className="row text-center">
           <div className="col-sm">
-            <form noValidate autoComplete="off">
-              <TextField fullWidth
-                margin="normal" id="outlined" label="Username" variant="outlined" /><br />
-              <TextField fullWidth
-                margin="normal" id="outlined" label="Passwordnya" variant="outlined" />
-            </form>
-          </div>
-          <div className="col-sm">
+            <h1>Hi!</h1>
+            <img src="/char1.svg"></img>
+            <h4>Kenali dirimu<br/>sekarang juga</h4>
             <Link href="/index">
-              <Button fullWidth variant="outlined" color="primary" disableElevation>
-                Masuk
+              <Button variant="outlined" color="primary" disableElevation>
+                Mulai Sekarang
               </Button>
             </Link>
           </div>
-
         </div>
       </div>
     </Layout>
